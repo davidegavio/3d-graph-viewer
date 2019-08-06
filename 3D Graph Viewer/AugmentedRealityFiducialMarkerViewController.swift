f2225496ad936086d221f12ee6d3b39cc39f6ee7
Davide Gavio <gaviodavide@gmail.com>
2019-08-06 14:15:18 +0200

    Tweaks.

--- 3D Graph Viewer/AugmentedRealityFiducialMarkerViewController.swift
+++ 3D Graph Viewer/AugmentedRealityFiducialMarkerViewController.swift
@@ -21,7 +21,7 @@ class AugmentedRealityFiducialMarkerViewController: UIViewController, ARSCNViewD
     var lastImagePosition:simd_float4x4?
     var lastReferenceImageDetected:ARReferenceImage?
     var isHorizontalPlaneDetected = false
-    var shouldScatterplotBePlacedUponImage = false
+    var shouldScatterplotBePlacedUponImage = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,12 +54,15 @@ class AugmentedRealityFiducialMarkerViewController: UIViewController, ARSCNViewD
     
     private func manageSceneStateChanges(withAnchor anchor: ARAnchor){
         if let imageAnchor = anchor as? ARImageAnchor{
-            lastImagePosition = imageAnchor.transform
-            let referenceImage = imageAnchor.referenceImage
-            lastReferenceImageDetected = referenceImage
-            print("Detected \(referenceImage.name!)")
-            placeScatterplotAt(position: lastImagePosition!)
-            shouldScatterplotBePlacedUponImage = false
+            if shouldScatterplotBePlacedUponImage{
+                lastImagePosition = imageAnchor.transform
+                let referenceImage = imageAnchor.referenceImage
+                lastReferenceImageDetected = referenceImage
+                print("Detected \(referenceImage.name!)")
+                addPlanes()
+                placeScatterplotAt(position: lastImagePosition!)
+                shouldScatterplotBePlacedUponImage = false
+            }
         }
         isImageDetected = true
         if let planeAnchor = anchor as? ARPlaneAnchor{
@@ -71,36 +74,41 @@ class AugmentedRealityFiducialMarkerViewController: UIViewController, ARSCNViewD
     }
     
     private func placeScatterplotAt(position: simd_float4x4){
-        addPlanes()
         for point in pointsToPlot{
             let sphere = SCNSphere(radius: 0.03)
             //let sphere = SCNSphere(radius: 1)
             let sphereNode = SCNNode(geometry: sphere)
             sphere.firstMaterial?.diffuse.contents = UIColor(red: CGFloat(Float(point.rColour) ?? 0), green: CGFloat(Float(point.gColour) ?? 255), blue: CGFloat(Float(point.bColour) ?? 255), alpha: 1)
+            
+            //sphereNode.position = SCNVector3((Float(point.xCoordinate)!+(lastImagePosition?.columns.3.x)!)/10, (Float(point.yCoordinate)!+(lastImagePosition?.columns.3.y)!)/10, (Float(point.zCoordinate)!+(lastImagePosition?.columns.3.z)!)/10)
             sphereNode.transform = SCNMatrix4(lastImagePosition!)
-            sphereNode.position = SCNVector3((Float(point.xCoordinate)!+(lastImagePosition?.columns.3.x)!)/10, (Float(point.yCoordinate)!+(lastImagePosition?.columns.3.y)!)/10, (Float(point.zCoordinate)!+(lastImagePosition?.columns.3.z)!)/10)
-            //sphereNode.position = SCNVector3(Float(point.xCoordinate)!/10, Float(point.yCoordinate)!/10, Float(point.zCoordinate)!/10)
-            //let scaleAction = SCNAction.scale(by: 0.05, duration: 0)
-            //jellyfishNode.runAction(scaleAction)
+            sphereNode.localTranslate(by: SCNVector3((Float(point.xCoordinate)!+(lastImagePosition?.columns.3.x)!)/10, (Float(point.yCoordinate)!+(lastImagePosition?.columns.3.y)!)/10, (Float(point.zCoordinate)!+(lastImagePosition?.columns.3.z)!)/10))
             augmentedRealityFiducialMarkerScatterplot.scene.rootNode.addChildNode(sphereNode)
             print(sphereNode.position)
             }
     }
     
     private func addPlanes(){
+        print("Adding planes")
         let verticalNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
         let horizontalNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
         let sideNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
         verticalNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "grid")
         verticalNode.opacity = 0.5
+        verticalNode.transform = SCNMatrix4(lastImagePosition!)
         horizontalNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "grid")
         horizontalNode.geometry?.firstMaterial?.isDoubleSided = true
         horizontalNode.eulerAngles = SCNVector3(90.toRadians, 0, 0)
         horizontalNode.opacity = 0.5
+        horizontalNode.transform = SCNMatrix4(lastImagePosition!)
         sideNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "grid")
         sideNode.eulerAngles = SCNVector3(0, 90.toRadians, 0)
         sideNode.geometry?.firstMaterial?.isDoubleSided = true
         sideNode.opacity = 0.5
+        sideNode.transform = SCNMatrix4(lastImagePosition!)
+        augmentedRealityFiducialMarkerScatterplot.scene.rootNode.addChildNode(verticalNode)
+        augmentedRealityFiducialMarkerScatterplot.scene.rootNode.addChildNode(horizontalNode)
+        augmentedRealityFiducialMarkerScatterplot.scene.rootNode.addChildNode(sideNode)
     }
     
 }
